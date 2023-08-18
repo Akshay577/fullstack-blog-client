b8c5cb6274f173c34537b7b3b8c8326ba83f3fc9
Akshay Nadig N R <akshaynadig.nr@spurtreetech.com>
2023-08-18 09:57:11 +0530

    fixed errors

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,42 +1,55 @@
-import React from 'react'
-import Logo from '../img/logo.png'
-import { Link } from 'react-router-dom'
+import React, { useContext } from "react";
+import { Link } from "react-router-dom";
+import { AuthContext } from "../context/authContext";
+import Logo from "../img/logo.png";
 
-function Navbar() {
+const Navbar = () => {
+  const { currentUser, logout } = useContext(AuthContext);
+
   return (
-    <div className='navbar'>
+    <div className="navbar">
       <div className="container">
         <div className="logo">
+          <Link to="/">
           <img src={Logo} alt="" />
+          </Link>
         </div>
         <div className="links">
-          <Link className='link' to='/?cat=art'>
+          <Link className="link" to="/?cat=art">
             <h6>ART</h6>
           </Link>
-          <Link className='link' to='/?cat=science'>
+          <Link className="link" to="/?cat=science">
             <h6>SCIENCE</h6>
           </Link>
-          <Link className='link' to='/?cat=technology'>
+          <Link className="link" to="/?cat=technology">
             <h6>TECHNOLOGY</h6>
           </Link>
-          <Link className='link' to='/?cat=cinema'>
+          <Link className="link" to="/?cat=cinema">
             <h6>CINEMA</h6>
           </Link>
-          <Link className='link' to='/?cat=design'>
+          <Link className="link" to="/?cat=design">
             <h6>DESIGN</h6>
           </Link>
-          <Link className='link' to='/?cat=food'>
+          <Link className="link" to="/?cat=food">
             <h6>FOOD</h6>
           </Link>
-          <span>John</span>
-          <span>Logout</span>
+          <span>{currentUser?.username}</span>
+          {currentUser ? (
+            <span onClick={logout}>Logout</span>
+          ) : (
+            <Link className="link" to="/login">
+              Login
+            </Link>
+          )}
           <span className="write">
-            <Link to='/write' className='link'>Write</Link>
+            <Link className="link" to="/write">
+              Write
+            </Link>
           </span>
         </div>
       </div>
     </div>
-  )
-}
+  );
+};
 
-export default Navbar
+export default Navbar;
